3deadd13ee260b5278b2d9d991719e2a521e82f1
Adrian Petrov <adrian@adrianpetrov.com>
2022-08-10 20:11:30 +0100

    first commit

---
+++ csvprovider/csvprovider_test.go
@@ -0,0 +1,8 @@
+package csvprovider
+
+import "testing"
+
+func TestCSVProvider(t *testing.T) {
+	t.Run("it should have 12 questions", func(t *testing.T) {
+	})
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/adrian-petrov/go-quiz
+
+go 1.19
